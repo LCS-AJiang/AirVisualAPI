37e60fc09fc59fcc6c9c95cb6ea5fb119892d10d
Audrey Jiang <ajiang@lcs.on.ca>
2023-04-20 08:49:05 -0400

    Initial Commit

---
+++ AirVisualAPI/AirVisualAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirVisualAPIApp.swift
+//  AirVisualAPI
+//
+//  Created by Audrey Jiang on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct AirVisualAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
